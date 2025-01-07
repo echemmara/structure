b055d678e3fd8abffe6609f8d19db35de34075dc
eChemmara <echemmara@gmail.com>
2025-01-07 16:55:18 +0800

    Create ProductList.js

---
+++ frontend/src/components/ProductList.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/products')
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default ProductList;
